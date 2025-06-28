536ced231f9d10c38957ebf3208942bd8dfb33a0
utkarsh <utkarshgupta.281@gmail.com>
2025-06-28 12:13:55 +0530

    add update user

--- Express/BlogApp/controllers/blog.controller.js
+++ Express/BlogApp/controllers/blog.controller.js
@@ -2,10 +2,12 @@ const blogCollection = require("../models/blog.model");
 const userCollection = require("../models/user.model");
 const asyncHandler = require("express-async-handler");
 const ErrorHandler = require("../utils/errorHandler.utils");
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
 const addBlog = asyncHandler(async (req, res) => {
   let userId = req.user._id;
-  console.log(req.user);
+  // console.log(req.user);
   let { title, content } = req.body;
 
   let newBlog = await blogCollection.create({
@@ -15,9 +17,13 @@ const addBlog = asyncHandler(async (req, res) => {
   });
   // $inc ==> { $inc: { key_name: = +/-value } }
   // await userCollection.updateOne({ _id: userId }, { $inc: { totalBlogs: 1 } });
-  await userCollection.findByIdAndUpdate(userId, { $inc: { totalBlogs: 1 } });
+  // await userCollection.findByIdAndUpdate(userId, { $inc: { totalBlogs: 1 } });
+  // await userCollection.findByIdAndUpdate(userId, { $push: { blogs: newBlog._id } });
 
-  await userCollection.findByIdAndUpdate(userId, { $push: { blogs: newBlog._id } });
+  await userCollection.findByIdAndUpdate(userId, {
+    $inc: { totalBlogs: 1 },
+    $push: { blogs: newBlog._id },
+  });
 
   res.status(201).json({
     success: true,
@@ -53,9 +59,63 @@ const fetchOneBlog = asyncHandler(async (req, res, next) => {
   });
 });
 
-const updateBlog = asyncHandler(async (req, res) => {});
+const updateBlog = asyncHandler(async (req, res, next) => {
+  let currentUserId = req.user._id;
+  let blogId = req.params.id; // (_id===blogId, createdBy===currentUserId)
+  let { title, content } = req.body;
 
-const deleteBlog = asyncHandler(async (req, res) => {});
+  let blog = await blogCollection.findOne({ _id: blogId, createdBy: currentUserId });
+  // let blog = await blogCollection.findOne({
+  //   $and: [{ _id: blogId }, { createdBy: currentUserId }],
+  // });
+  if (!blog) return next(new ErrorHandler("blog not found", 404));
+
+  // await blogCollection.updateOne({ _id: blogId }, { $set: { title, content } });
+  let updatedBlog = await blogCollection.findByIdAndUpdate(
+    blogId,
+    { title, content },
+    {
+      new: true, // return the updated document
+      runValidators: true, // validates the updated document
+    }
+  );
+  res.status(200).json({
+    success: true,
+    message: "blog updated successfully",
+    updatedBlog,
+  });
+});
+
+const deleteBlog = asyncHandler(async (req, res, next) => {
+  let currentUserId = req.user._id;
+  let blogId = req.params.id; // (_id===blogId, createdBy===currentUserId)
+
+  let blog = await blogCollection.findOne({ _id: blogId, createdBy: currentUserId });
+  if (!blog) return next(new ErrorHandler("blog not found", 404));
+
+  // let deletedBlog = await blogCollection.deleteOne({ _id: blogId, createdBy: currentUserId });
+  // //? update the totalBlogs of the user
+  // await userCollection.findByIdAndUpdate(req.user._id, { $inc: { totalBlogs: -1 } });
+  //? update the blogs array of the user
+  // await userCollection.findByIdAndUpdate(req.user._id, { $pull: { blogs: ObjectId(blogId) } });
+
+  let myUser = req.user;
+
+  console.log(myUser.blogs);
+
+  let updatedArray = myUser.blogs.filter((id) => {
+    return new mongoose.Types.ObjectId(blogId).equals(id);
+  });
+
+  console.log(updatedArray);
+  console.log(myUser.blogs);
+
+  res.status(200).json({
+    success: true,
+    message: "blog deleted successfully",
+    blog,
+  });
+});
 
 module.exports = {
   addBlog,
--- Express/BlogApp/routes/blog.routes.js
+++ Express/BlogApp/routes/blog.routes.js
@@ -1,5 +1,11 @@
 const { Router } = require("express");
-const { addBlog, fetchAllBlogs, fetchOneBlog } = require("../controllers/blog.controller");
+const {
+  addBlog,
+  fetchAllBlogs,
+  fetchOneBlog,
+  updateBlog,
+  deleteBlog,
+} = require("../controllers/blog.controller");
 const { authenticate } = require("../middlewares/auth.middleware");
 
 const router = Router();
@@ -7,5 +13,7 @@ const router = Router();
 router.post("/add", authenticate, addBlog); // injected authenticate middleware
 router.get("/blogs", fetchAllBlogs);
 router.get("/blog/:id", fetchOneBlog);
+router.patch("/blog/:id", authenticate, updateBlog);
+router.delete("/blog/:id", authenticate, deleteBlog);
 
 module.exports = router;
